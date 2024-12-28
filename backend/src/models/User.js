9a3d4e37009cb31ae0fd67ea3609cb5594019409
Dev-Putra12 <putraagus1204@gmail.com>
2024-12-28 20:24:47 +0800

    integration add friend

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -58,7 +58,24 @@ const userSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  friends: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 });
+
+userSchema.index({ email: 1 }, { unique: true });
+userSchema.methods.isFriendWith = function(userId) {
+  return this.friends && this.friends.includes(userId);
+};
+
+userSchema.methods.addFriend = function(friendId) {
+  if (!this.friends) this.friends = [];
+  if (!this.friends.includes(friendId)) {
+    this.friends.push(friendId);
+  }
+  return this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);
